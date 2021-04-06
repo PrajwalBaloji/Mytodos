a4191d6a094544954d2e406e0b6adfc7248d92fb
PrajwalBaloji <81876675+PrajwalBaloji@users.noreply.github.com>
2021-04-06 22:00:34 +0530

    fetching,updating,and deleting data from firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import "./App.css";
 import TextField from "@material-ui/core/TextField";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
+import firebase from 'firebase'
+import {db} from './firebase_config'
+import TodoListItem from './Todo'
+
 
 function App() {
+  const [todos,setTodos]=useState([])
   const [todoInput, settodoInput] = useState("");
+
+  useEffect(() => {
+  
+   getTodos();
+  }, [])
+
+  const getTodos=()=>{
+     db.collection('todos').onSnapshot(function(querySnapshot){
+       setTodos(
+        querySnapshot.docs.map((docs)=>({
+          id:docs.id,
+          todo:docs.data().todo,
+          inprogress:docs.data().inprogress,
+        }))
+       )
+       
+     })
+  }
 
   const addTodo =(e)=>{
     
     e.preventDefault()
      
+    db.collection("todos").add({
+      inprogress:true,
+      timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+      todo:todoInput
+    })
+
+    settodoInput("");
     
   }
+
+
   return (
     <div className="App">
       <h1> My Todos App </h1>
@@ -20,10 +52,19 @@ function App() {
         id="standard-basic"
         label="Write a todo"
         className='TextField'
+        style={{maxWidth:'300px',width:'90vw'}}
+        value={todoInput}
         onChange={(e) => settodoInput(e.target.value) }
       />
-      <Button type='submit' variant="contained" onClick={addTodo}>Add</Button>
+      <Button type='submit' variant="contained" onClick={addTodo} style={{display:'none'}}>Add</Button>
       </form>
+      {
+        todos.map((todo,index)=>{
+          return(
+            <TodoListItem key={index} todo={todo.todo} inprogress={todo.inprogress} id={todo.id}/>
+          )
+        })
+      }
     </div>
   );
 }
